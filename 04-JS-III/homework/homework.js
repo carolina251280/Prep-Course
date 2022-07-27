2f3e81e08e9c13a16aed901691d855a96c7ba8c8
carolina251280 <carolinamarquez.gcg@gmail.com>
2022-07-27 19:43:45 -0300

    Test 04 JS III completo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -117,13 +117,26 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length == 0){
+      return 0;
+    }
+  var mult=1; 
+  for(var i=0; i < arguments.length ; i++){
+      mult= mult*arguments[i];
+  }
+  return mult;
 }
-
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var acc=0;
+  for (var i=0 ; i < arreglo.length ; i++){
+    if (arreglo[i]> 18){
+      acc = acc + 1; 
+    }
+  }
+  return acc;
 }
 
 
@@ -132,6 +145,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (numeroDeDia == 1 || numeroDeDia ==7){
+    return "Es fin de semana";
+  } else{
+    return "Es dia Laboral";
+  }
   
 } 
 
@@ -140,6 +158,13 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var i=n.toString();
+  if (i.charAt(0) == 9){
+    return true;
+  }
+  else{
+    return false;
+  }
   
 }
 
@@ -148,6 +173,18 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var iguales= arreglo[0];
+  var acc=0;
+  for(var i=0 ; i < arreglo.length ; i++){
+    if (arreglo[i]=== iguales){
+       acc++;
+    }
+  }
+  if (acc == arreglo.length){
+    return true;
+  }else{
+    return false;
+  }
   
 } 
 
@@ -157,13 +194,37 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+  var nuevo=[];
+  for (var i=0 ; i < array.length ; i++){
+    if (array[i] === "Enero"){
+      nuevo.push(array[i]);
+    }
+    if (array[i] === "Marzo"){
+      nuevo.push(array[i]);
+    }
+    if (array[i] === "Noviembre"){
+      nuevo.push(array[i]);
+    } 
+  }
+  if (nuevo.length == 3){
+    return nuevo;
+  }else {
+    return "No se encontraron los meses pedidos"
+  }
 
+}
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
-  // Tu código:
+  // Tu código: 
+  var nuevoArray=[];
+  for (var i= 0 ; i < array.length ; i++){
+    if (array[i] > 100 && array[i] <= 200){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 
@@ -175,6 +236,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var nuevoArray=[];
+  var acc=numero;
+  var aux=0;
+  for(var i=0 ; i < 10 ;i++){
+    acc=acc+2;
+    if(acc === aux){ break;}
+    aux++;
+    nuevoArray.push(acc);
+
+  }
+  if(acc === aux) {return "Se interrumpió la ejecución"};
+  return nuevoArray;
+ 
 }
 
 
@@ -185,6 +259,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+
+
+var nuevoArray=[];
+var suma=numero;
+for(var i=1 ; i <= 10 ; i++){
+  if(i === 5){ continue;}
+  suma=suma+2;
+  nuevoArray.push(suma);
+
+  }
+ return nuevoArray; 
+
 }
 
 
@@ -212,4 +298,4 @@ module.exports = {
   mayorACien,
   breakStatement,
   continueStatement
-};
+}
